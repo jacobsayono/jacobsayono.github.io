6ebd2f42de53685a4cebb030fac8b1d796b6d3f1
Jacob Sayono <jacobsayono@JS.localdomain>
2022-11-28 05:38:00 -0800

    so good

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let btn = document.querySelector(".darkmode");
 btn.addEventListener("click", () => {
   if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
     btn.innerHTML = `<i class="fas fa-sun"></i>`;
-    btn.style.color = "orange";
-    btn.style.backgroundColor = "rgb(43, 43, 43)";
+    btn.style.color = "white";
+    btn.style.backgroundColor = "transparent";
     document.body.classList.add("darkmode");
   } else {
     btn.innerHTML = `<i class="fas fa-moon"></i>`;
     btn.style.color = "black";
-    btn.style.backgroundColor = "white";
+    btn.style.backgroundColor = "transparent";
     document.body.classList.remove("darkmode");
   }
 });
