ba943762998a7a5b64d7cd75dfbf539fd116573b
Jacob <jrsayono@gmail.com>
2022-12-02 12:29:09 -0800

    image crop

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
-  // let captionText = document.getElementById("caption");
+  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
