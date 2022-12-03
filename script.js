9d24de3a307ff54360ff193fec27698ee65c137e
Jacob <jrsayono@gmail.com>
2022-12-02 19:31:33 -0800

    big commit big checkpoint project section done

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
+/* display navigation buttons at bottom of project images */
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -74,4 +75,21 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
+}
+
+/* `see more` feature*/
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Show more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Show less"; 
+    moreText.style.display = "inline";
+  }
 }
