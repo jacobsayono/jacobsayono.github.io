19d2095468538e0be0478423ccde9ec6b462dd5e
Jacob Sayono <jrsayono@gmail.com>
2022-12-08 01:26:41 -0800

    remove currentSlide

--- script.js
+++ script.js
@@ -27,11 +27,6 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
