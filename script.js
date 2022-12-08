e9ed3eb02eb0f83a29267720a68b3ce74ed96f3b
Jacob Sayono <jrsayono@gmail.com>
2022-12-08 01:18:40 -0800

    clean up script.js for unused caption portion

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+  // let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -47,7 +47,7 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  // captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
 /* display navigation buttons at bottom of project images */
